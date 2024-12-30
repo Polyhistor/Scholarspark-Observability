ddf28f669852840353ebf45271ee9c22bbba8d75
Pouya <pouya.ataei.7@gmail.com>
2024-12-30 17:07:56 +1300

    added a test

--- tests/test_scholarSparkObservability.py
+++
@@ -1,8 +0,0 @@
-"""
-Test suite for scholarSparkObservability
-"""
-from scholarSparkObservability import ScholarSparkObservability
-
-def test_example_method():
-    client = ScholarSparkObservability()
-    assert client.example_method() == "Hello from scholarSparkObservability!"
